97429cbe0fbec4cdbe4dea2f9ba1c45ad5901a54
Leo Roos <leor.email@gmail.com>
2014-12-21 16:29:55 -0800

    refactored initialization

--- code/bplustree/bplustree.rb
+++ code/bplustree/bplustree.rb
@@ -1,9 +1,10 @@
 class Node
-  attr_accessor :key_array, :bplusarray, :is_leaf
+  attr_accessor :key_array, :bplusarray, :is_leaf, :is_root
   
-  def initialize(bfactor)
+  def initialize(bfactor, is_root=false)
     @key_array = Array.new(bfactor-1)
     @bplusarray = Array.new(bfactor)
+    self.is_root = is_root
   end  
   
   def get key  
@@ -12,11 +13,18 @@ class Node
     end  
   end
   
-  # def insert data
-  #   key_array.each do |k, i|
-  #     return bplusarray[i] if key == k
-  #   end
-  # end
+  def insert data
+    # if is_root
+    #   if  @key_array.empty?
+    #     @key_array[0] = data.first
+    #     @bplusarray[0] = data.last
+    #   else
+    #     
+    #   end
+    # else
+    #   raise "foo"
+    # end
+  end
   
   def to_s
     "key_array = #{key_array}\n bplusarray= #{bplusarray}"
@@ -28,23 +36,30 @@ class Bplustree
   
   attr_accessor :bfactor, :root
   
-  def initialize bfactor, first, second
+  def initialize bfactor
     @bfactor = bfactor
-    @root = Node.new(bfactor)
+  end
+
+  def initialize_root k1,v1,k2,v2
+    @root = Node.new(bfactor,true)
+    @root.is_leaf = true
+    @root.is_root = true
     # TODO: Refactor first into insert
-    if first.first[0] < second.first[0]
-      @root.key_array[0]= first.first[0]
-      @root.bplusarray[0]= first.first[1]
-      @root.key_array[1] =second.first[0]
-      @root.bplusarray[1] =second.first[1]
+    insert_sorted k1,v1,k2,v2
+  end
+
+  def insert_sorted k1,v1,k2,v2
+    if k1 < k2
+      @root.key_array[0]= k1
+      @root.bplusarray[0]= v1
+      @root.key_array[1] = k2
+      @root.bplusarray[1] = v2
     else
-      @root.key_array[0] =second.first[0]
-      @root.bplusarray[0] =second.first[1]
-      @root.key_array[1]= first.first[0]
-      @root.bplusarray[1]= first.first[1]
+      @root.key_array[0] = k2
+      @root.bplusarray[0] = v2
+      @root.key_array[1]= k1
+      @root.bplusarray[1]= v1
     end
-    # -----
-    @root.is_leaf = false
   end
 
   def to_s
--- code/bplustree/bplustree_test.rb
+++ code/bplustree/bplustree_test.rb
@@ -2,20 +2,35 @@ require 'minitest/autorun'
 require_relative 'bplustree'
 
 class BplustreeTest < MiniTest::Unit::TestCase
+  KV = Struct.new :key, :value
+
   def test_initialize_btree
     branching_factor = 3
-    first = {"1"=> "one"}
-    second ={"2"=> "My Second Data"}
-    b  = Bplustree.new(branching_factor, first, second )
-    assert_equal false, b.root.is_leaf
-    p b.root
-    assert_equal first.first[1], b.get(first.first[0])
-    #assert_equal "root{[1,2][#{first},#{second}]}", b.to_s
+    first = KV.new "1","one"
+    second = KV.new "2", "My Second Data"
+    b  = Bplustree.new(branching_factor)
+    b.initialize_root first.key, first.value, second.key, second.value
+
+    assert b.root.is_leaf
+    assert b.root.is_root, 'first node should be root'
+
+    assert_equal first.value, b.get(first.key)
+    assert_equal second.value, b.get(second.key)
   end
   
-  # TODO
-  def test_insert_some_data
-  end
+  # # TODO
+  # def test_insert_some_data
+  #   first = {"1"=> "one"}
+  #   second ={"2"=> "My Second Data"}
+  #   b  = Bplustree.new(3, first, second )
+  #   third =  KV.new "4", data"
+  #   b.insert third
+  #   assert_equal false, b.root.is_leaf
+  #   assert b.root.is_root, 'first node should be root'
+  #   assert_equal first.first[1], b.get(first.first[0])
+  #   assert_equal second.first[1], b.get(second.first[0])
+  #   assert_equal third.first[1], b.get(third.first[0])
+  # end
   
   
 end
